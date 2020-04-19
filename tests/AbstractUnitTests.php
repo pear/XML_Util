a76d534ad0c3bd4da7e164185c5a7032ec812c23
Chuck Burgess <ashnazg@php.net>
2020-04-19 11:06:04 -0500

    remove unused setup()

--- tests/AbstractUnitTests.php
+++ tests/AbstractUnitTests.php
@@ -9,9 +9,5 @@ if (!class_exists('PHPUnit_Framework_TestCase')) {
 
 abstract class AbstractUnitTests extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        // ensure the class is defined, and thus its constants are declared
-        new XML_Util();
-    }
+
 }
